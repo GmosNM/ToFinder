b03aef42c249a0d8c1d438e4c72290e18ad16e11
GmosNM <roleyking6@gmail.com>
2023-03-27 22:35:57 -0700

    replace &str to &Path

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 use std::io::{BufRead, BufReader, Write};
-use std::path::PathBuf;
-use std::path::Path;
+use std::path::{PathBuf, Path};
 
 fn format_file(file_path: &str) -> std::io::Result<()> {
     let file = File::open(file_path)?;
@@ -34,7 +33,7 @@ fn format_file(file_path: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-fn read_file(file_path: &str) -> String {
+fn read_file(file_path: &Path) -> String {
     let mut file = OpenOptions::new().read(true).open(file_path).expect("ERROR: could not read file");
     let metadata = file.metadata().expect("error");
     let mut permissions = metadata.permissions();
@@ -47,7 +46,7 @@ fn read_file(file_path: &str) -> String {
     buffer
 }
 
-fn add_todos_to_file(file_path: &str, mut todos_file: &File) {
+fn add_todos_to_file(file_path: &Path, mut todos_file: &File) {
     let content = read_file(&file_path);
     let lines = content.lines();
     todos_file.write_all(b"").expect("ERROR: no file");
@@ -67,19 +66,29 @@ fn add_todos_to_file(file_path: &str, mut todos_file: &File) {
     }
 }
 
-fn read_dir(dir_path: &str) {
-    let mut todos_file = OpenOptions::new().append(false).create(true).write(true).open("TODOS").unwrap();
+
+fn read_dir(dir_path: &Path) {
+    let mut todos_file = OpenOptions::new()
+        .append(false)
+        .create(true)
+        .write(true)
+        .open("TODOS")
+        .unwrap();
+
     for entry in std::fs::read_dir(dir_path).expect("ERROR: could not read directory") {
         if let Ok(entry) = entry {
             if entry.file_type().unwrap().is_file() {
-                add_todos_to_file(&entry.path().display().to_string(), &mut todos_file);
+                add_todos_to_file(&entry.path(), &mut todos_file);
             } else if entry.file_type().unwrap().is_dir() {
-                read_dir(&entry.path().display().to_string());
+                read_dir(&entry.path());
             }
         }
     }
+
     format_file("TODOS").expect("ERROR: could not format the file");
 }
+
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
@@ -89,11 +98,11 @@ fn main() {
 
     let path = Path::new(&args[1]);
     if path.is_dir() {
-        read_dir(&args[1]);
+        read_dir(path);
     } else if path.is_file() {
         let mut todos_file = OpenOptions::new().append(true).create(true).write(true).open("TODOS").unwrap();
         todos_file.write_all(b"").expect("ERROR: could not clear file");
-        add_todos_to_file(&args[1], &mut todos_file);
+        add_todos_to_file(path, &mut todos_file);
         format_file(&args[1]).expect("ERROR: could not format the file");
         println!("File formatted successfully!");
     } else {
