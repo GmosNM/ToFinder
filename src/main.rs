5e822a7ab5a6ec0dcb039be5de60e1ba9f5b7fd6
GmosNM <roleyking6@gmail.com>
2023-03-27 18:20:19 -0700

    added file name
    "when adding the todos to the TODOS file it add the file name and the
    line and format it after"

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
+use std::path::PathBuf;
 
-
-fn format_file(file_path: String) {
-    let content: String = read_file(file_path.clone());
+fn format_file(file_path: &str) {
+    let content = read_file(&file_path);
     let formatted_content = content.replace("\t", "    ");
-    let mut file = File::create(file_path).expect("ERROR: could not create file");
+    let mut file = File::create(&file_path).expect("ERROR: could not create file");
     file.write_all(formatted_content.as_bytes())
         .expect("ERROR: could not write to file");
 }
 
-
-fn read_file(file_path: String) -> String {
-    let mut file =  OpenOptions::new().read(true).open(file_path).expect("ERROR: could not read file");
-    let metadata = file.metadata();
-    let mut permissions = metadata.expect("error").permissions();
+fn read_file(file_path: &str) -> String {
+    let mut file = OpenOptions::new().read(true).open(file_path).expect("ERROR: could not read file");
+    let metadata = file.metadata().expect("error");
+    let mut permissions = metadata.permissions();
     permissions.set_readonly(true);
     let mut buffer = String::new();
     if let Err(e) = file.read_to_string(&mut buffer) {
@@ -24,29 +23,36 @@ fn read_file(file_path: String) -> String {
     return buffer;
 }
 
-fn add_todos_to_file(file_path: String) {
-    let content: String = read_file(file_path);
-    let lines: Vec<&str> = content.lines().collect();
-    let mut file = OpenOptions::new().write(true).create(true).write(true).open("TODOS").unwrap();
-    for (i, line) in lines.iter().enumerate() {
+fn add_todos_to_file(file_path: &str) {
+    let content = read_file(file_path);
+    let lines = content.lines();
+    let mut todos_file = OpenOptions::new().append(true).create(true).write(true).open("TODOS").unwrap();
+    if todos_file.metadata().unwrap().len() == 0 {
+        todos_file.write_all(b"TODOS:\n").expect("ERROR: could not write to file");
+    }
+    for (i, line) in lines.enumerate() {
         if line.contains("//todo") || line.contains("//TODO") || line.contains("// TODO") {
-            let todo = format!("Line {}: {}\n", i + 1, line);
-            file.write_all(todo.as_bytes())
+            let path_buf = PathBuf::from(file_path);
+            let file_name = path_buf.file_name().unwrap().to_str().unwrap();
+            let todo = format!("{}:{}: {}\n", file_name, i + 1, line);
+            todos_file.write_all(todo.as_bytes())
                 .expect("ERROR: could not write to file");
             println!("{}", todo);
         }
     }
 }
 
-fn read_dir(dir_path: String) {
-    let paths = std::fs::read_dir(dir_path).expect("ERROR: could not read directory");
-    for path in paths {
-        let file_path = path.unwrap().path().display().to_string();
-        add_todos_to_file(file_path.clone());
-        format_file("TODOS".to_string());
+fn read_dir(dir_path: &str) {
+    for entry in std::fs::read_dir(dir_path).expect("ERROR: could not read directory") {
+        if let Ok(entry) = entry {
+            if entry.file_type().unwrap().is_file() {
+                let file_path = entry.path().display().to_string();
+                add_todos_to_file(&file_path);
+            }
+        }
     }
+    format_file("TODOS");
 }
-
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -55,6 +61,5 @@ fn main() {
         return;
     }
 
-    let dir_path = &args[1];
-    read_dir(dir_path.clone());
+    read_dir(&args[1]);
 }
